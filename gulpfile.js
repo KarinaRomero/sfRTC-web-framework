654e4ccbaa0f8f8a18dcaa5f394f13a6d530a60c
fenix <betzy_skamm@hotmail.com>
2018-08-21 13:09:25 -0700

    added task to compress

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,10 @@
 const gulp = require('gulp');
-const uglify = require('gulp-uglify');
+const minify = require('gulp-minify');
 var gutil = require('gulp-util');
-
-/*gulp.task('compress', ()=> (
-    gulp.src('./js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/')))
-);*/
-
 
 gulp.task('compress', function() {
     return gulp.src('./js/*.js')
-        // .pipe(concat('script.js'))
-        .pipe(uglify())
+        .pipe(minify())
         .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
         .pipe(gulp.dest('./dist/'))
-    });
+});
