6c49a215a495ed30dd7d2e2b38561f7d110b57a9
fenix <betzy_skamm@hotmail.com>
2018-10-21 02:22:31 -0700

    updating gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,29 @@
-const gulp = require('gulp');
-const rename = require("gulp-rename");
-const uglify = require('gulp-uglify-es').default;
-const log = require('fancy-log');
-const color = require('ansi-colors');
-const zip = require('gulp-zip');
+const gulp = require('gulp')
+const rename = require('gulp-rename')
+const uglify = require('gulp-uglify-es').default
+const log = require('fancy-log')
+const color = require('ansi-colors')
+const zip = require('gulp-zip')
 
 gulp.task('build', () =>
-    gulp.src('./js/clientWebRTC.js')
-        .pipe(rename("sfRTC.min.js"))
-        .pipe(uglify())
-        .on('error', function (err) {log(color.red('[Error]'), err.toString());})
-        .pipe(gulp.dest('./dist/'))
-);
+  gulp
+    .src('./js/clientWebRTC.js')
+    .pipe(rename('sfRTC.min.js'))
+    .pipe(uglify())
+    .on('error', function(err) {
+      log(color.red('[Error]'), err.toString())
+    })
+    .pipe(gulp.dest('./dist/'))
+)
 
 gulp.task('compress', () =>
-    gulp.src('dist/sfRTC.min.js')
-        .pipe(zip('sfRTC.zip'))
-        .pipe(gulp.dest('dist'))
-);
+  gulp
+    .src('dist/sfRTC.min.js')
+    .pipe(zip('sfRTC.zip'))
+    .pipe(gulp.dest('dist'))
+)
 
-gulp.task('package', gulp.series('build', 'compress'));
+gulp.task('copy', () => gulp.src('./package.json').pipe(gulp.dest('./dist')))
+
+gulp.task('package', gulp.series('build', 'compress'))
+gulp.task('build-for-npm', gulp.series('build', 'copy'))
