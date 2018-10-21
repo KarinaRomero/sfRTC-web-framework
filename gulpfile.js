d940d1c61c6734e59c166de2b025c6bb36dc0cf6
fenix <betzy_skamm@hotmail.com>
2018-10-20 22:44:20 -0700

    distribution tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,24 @@ const rename = require("gulp-rename");
 const uglify = require('gulp-uglify-es').default;
 const log = require('fancy-log');
 const color = require('ansi-colors');
+const zip = require('gulp-zip');
 
-gulp.task('compress', function() {
-    return gulp.src('./js/clientWebRTC.js')
+gulp.task('build', () =>
+    gulp.src('./js/clientWebRTC.js')
         .pipe(rename("sfRTC.min.js"))
         .pipe(uglify())
         .on('error', function (err) {log(color.red('[Error]'), err.toString());})
         .pipe(gulp.dest('./dist/'))
-});
+);
+
+gulp.task('compress', () =>
+    gulp.src('dist/sfRTC.min.js')
+        .pipe(zip('sfRTC.zip'))
+        .pipe(gulp.dest('dist'))
+);
+
+/*gulp.task('package', () =>
+    gulp.start('build'),
+    gulp.start('compress')
+);*/
+gulp.task('package', gulp.series('build', 'compress'));
